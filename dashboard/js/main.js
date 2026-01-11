44ec07b801dadf05820e70ae805cab09571831d1
h3ik0 <98340313+ilostmypassword@users.noreply.github.com>
2026-01-11 15:08:30 +0000

    Change loadLogs to await loadLogs for async handling

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,7 +1,7 @@
 import { loadLogs, setupSearch } from './searchEngine.js';
 import { displayResults, setupExportButton } from './searchDisplay.js';
 
-loadLogs();
+await loadLogs();
 setupSearch((filteredLogs, searchTerms) => {
     displayResults(filteredLogs, searchTerms);
     setupExportButton(filteredLogs);
