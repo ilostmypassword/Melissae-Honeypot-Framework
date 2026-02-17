88cc3bd3ee05e7144f0aaf788f1b76cdd5eef96b
h3ik0 <98340313+ilostmypassword@users.noreply.github.com>
2026-02-17 09:57:54 +0000

    Delete dashboard/js directory

--- dashboard/js/main.js
+++
@@ -1,8 +0,0 @@
-import { loadLogs, setupSearch } from './searchEngine.js';
-import { displayResults, setupExportButton } from './searchDisplay.js';
-
-await loadLogs();
-setupSearch((filteredLogs, searchTerms) => {
-    displayResults(filteredLogs, searchTerms);
-    setupExportButton(filteredLogs);
-});
